c64c60284959a0a3cf15883d0d6ce87678d4b5e5
SikangSun <89489927+SikangSun@users.noreply.github.com>
2022-12-17 00:49:20 -0500

    dist

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+export default {
+    plugins: [
+        svelte({
+                // This tells svelte to run some preprocessing
+                preprocess: sveltePreprocess({
+                        postcss: true,  // And tells it to specifically run postcss!
+                }),
+
+                // Omitted for brevity
+        }),
+
+        // Omitted for brevity
+],
+watch: {
+        clearScreen: false
+}
+}
---
+++ src/TailwindCSS.svelte
@@ -0,0 +1,11 @@
+<script>
+   
+</script>
+<style global>
+@import "tailwindcss/base";
+
+@import "tailwindcss/components";
+
+@import "tailwindcss/utilities";
+@import "tailwindcss/colors";
+</style>
